731547d817210fcb6514a7cc3192e269d80f938b
Jimmy Rando <jsturat46925@gmail.com>
2021-12-15 16:53:30 -0500

    updated task display  text

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,27 @@ var tasksToDoE1= document.querySelector("#tasks-to-do");
 var createTaskHandler= function(event)
 {
     event.preventDefault();
+    var taskNameInput= document.querySelector("input[name='task-name']").value;
+    var taskTypeInput= document.querySelector("select[name='task-type']").value;
 
+    // create list item
     var listItemE1= document.createElement("li");
     listItemE1.className= "task-item";
-    listItemE1.textContent= "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);   
+    
+    // create div to hold task info and add to list item
+    var taskInfoE1= document.createElement("div");
+
+    // give it a class name
+    taskInfoE1.className= "task-info";
+
+    // add HTML content to div
+    taskInfoE1.innerHTML= "<h3 class= 'task-name'>"+taskNameInput
+        +"</h3><span class='task-type'>"+taskTypeInput+"</span>";
+    
+    listItemE1.appendChild(taskInfoE1);
+    
+    // add entire list item to list
+    tasksToDoE1.appendChild(listItemE1);
 };
 
 
